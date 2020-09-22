f0079801f2f6d8da512d72143433d61ab48978b8
Joab Moura <66380426+dev-joh@users.noreply.github.com>
2020-09-22 15:03:48 -0300

    Add files via upload

---
+++ Cores.py
@@ -0,0 +1,7 @@
+nome="Joab"
+cores = {"limpa" : "\033[m" , 
+        "azul" : "\033[34m" , 
+        "amarelo" : "\033[33m" , 
+        "vermelho" : "\033[31m" }
+print("Olá {}{}{}".format(cores["vermelho"],nome,cores["limpa"]))
+
